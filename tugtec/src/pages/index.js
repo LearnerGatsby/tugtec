95962754e5f27bbe14a090352df4598fdfaf6b1a
LearnerGatsby <learner.gatsby@gmail.com>
2022-02-07 15:03:13 +0800

    index commend

--- tugtec/src/pages/index.js
+++ tugtec/src/pages/index.js
@@ -165,7 +165,7 @@ const IndexPage = () => {
               </a>
               {link.badge && (
                 <span style={badgeStyle} aria-label="New Badge">
-                  NEW Test!
+                  NEW Test AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA!
                 </span>
               )}
               <p style={descriptionStyle}>{link.description}</p>
